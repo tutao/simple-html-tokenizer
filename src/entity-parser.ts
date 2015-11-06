d50ecb03e060f464a6145b69f1ba8d9454dfa8d0
nfrasser <nfrasser@gmail.com>
2015-11-06 15:56:23 -0500

    Don't convert HTML entities to literal characters
    - Leave them as is for the linkify plugin to reduce file size where
    - converting to literal characters and back to entities is not required
    - Only exception is the nbsp character
    - Consistent variable name for HTML5NamedCharRefs

--- src/entity-parser.ts
+++ src/entity-parser.ts
@@ -13,15 +13,15 @@ export default class EntityParser {
     }
     let matches = entity.match(HEXCHARCODE);
     if (matches) {
-      return String.fromCharCode(parseInt(matches[1], 16));
+      return  `&#x${matches[1]};`;
     }
     matches = entity.match(CHARCODE);
     if (matches) {
-      return String.fromCharCode(parseInt(matches[1], 10));
+      return `&#${matches[1]};`;
     }
     matches = entity.match(NAMED);
     if (matches) {
-      return this.named[matches[1]];
+      return  this.named[matches[1]] || `&${matches[1]};`;
     }
   }
 }
